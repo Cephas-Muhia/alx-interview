0aa3a584b9479ee0179aa81236183ffd80c3da09
Cephas-Muhia <cephaschronixx@gmail.com>
2024-08-01 15:03:01 +0300

    lockboxes implementation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,16 +14,16 @@ def canUnlockAll(boxes):
         bool: True if all boxes can be opened, else False.
     """
     n = len(boxes)
-    opened = [False] * n
-    opened[0] = True
-    keys = [0]
+    opened = [False] * n  # Keep track of which boxes are opened
+    opened[0] = True  # The first box is already open
+    keys = [0]  # Start with the key to the first box
 
     while keys:
-        key = keys.pop()
-        for k in boxes[key]:
-            if k < n and not opened[k]:
-                opened[k] = True
-                keys.append(k)
+        key = keys.pop()  # Take the last key you found
+        for k in boxes[key]:  # Look at all keys in the current box
+            if k < n and not opened[k]:  # If the key opens a box we haven't opened yet
+                opened[k] = True  # Mark that box as opened
+                keys.append(k)  # Add the key to our list of keys
 
-    return all(opened)
+    return all(opened)  # If all boxes are opened, return True
 
